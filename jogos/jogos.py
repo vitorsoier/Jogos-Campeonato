17273ef1b90d91acd9e1acc6bdbab70b70256a03
Vitor Soier <vitorsoier1@gmail.com>
2022-10-08 22:51:06 -0300

    add git ignore

--- jogos/jogos.py
+++ jogos/jogos.py
@@ -6,6 +6,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+
 class Crawler:
     content = None
 
@@ -51,42 +52,77 @@ class CbfJogos(Crawler):
         data = data_id[0]
         id = data_id[1]
         id = re.search("[0-9]+", id)[0]
-        self.data =  data
+        self.data = data
         self.id = id
 
     def extractor_rodada(self, lista):
         rodada = lista.find('h3').get_text()
         rodada = re.search("[0-9]+", rodada)[0]
         self.rodada = rodada
-    
+
     def extractor_local_jogo(self, lista):
-        local = lista.find('span', attrs= {'partida-desc text-1 color-lightgray block uppercase text-center'}).get_text()
+        local = lista.find('span', attrs={
+                           'partida-desc text-1 color-lightgray block uppercase text-center'}).get_text()
         local = re.sub(r"^\s+", "", local,
-                         flags=re.UNICODE | re.MULTILINE)
+                       flags=re.UNICODE | re.MULTILINE)
         local = re.sub("\n[a-z]+ [a-z]+ [a-z]+ [a-z]+\n|\n[a-z]+ [a-z]+ [a-z]+\n", "", local,
-        flags=re.UNICODE | re.IGNORECASE)
+                       flags=re.UNICODE | re.IGNORECASE)
         self.local = local
-    
+
     def extractor_mandante(self, lista):
-        info_mandante = lista.find('div', attrs = {'time pull-left'})
-        nome = info_mandante.find('img')['title']
+        info_mandante = lista.find('div', attrs={'time pull-left'})
+        nome = info_mandante.find('img')['title'].split(' - ')[0]
         imagem = info_mandante.find('img')['src']
         sigla = info_mandante.get_text()
         sigla = re.sub(r"^\s+", "", sigla,
-                         flags=re.UNICODE | re.MULTILINE ).replace('\n', '')
-        mandante = {'nome': nome, 'imagem_url': imagem, 'sigla' : sigla}
+                       flags=re.UNICODE | re.MULTILINE).replace('\n', '')
+        mandante = {'nome': nome, 'imagem_url': imagem, 'sigla': sigla}
         self.mandante = mandante
 
+    def extractor_visitante(self, lista):
+        info_mandante = lista.find('div', attrs={'time pull-right'})
+        nome = info_mandante.find('img')['title'].split(' - ')[0]
+        imagem = info_mandante.find('img')['src']
+        sigla = info_mandante.get_text()
+        sigla = re.sub(r"^\s+", "", sigla,
+                       flags=re.UNICODE | re.MULTILINE).replace('\n', '')
+        visitante = {'nome': nome, 'imagem_url': imagem, 'sigla': sigla}
+        self.visitante = visitante
+
+    def extractor_placar(self, lista):
+        placar = lista.find(
+            'span', attrs={'bg-blue color-white label-2'})
+        if placar is None:
+            placar = [0, 0]
+        else:
+            placar = placar.get_text().split(' x ')
+        self.placar = placar
+
+    def extractor_detalhes(self, lista):
+        detalhes = lista.find('span', attrs={
+            'partida-desc text-1 color-lightgray block uppercase text-center'})
+        link_detalhes = detalhes.find('a')['href']
+        self.link_detalhes = link_detalhes
+
+    def extractor_all(self, jogo):
+        self.extractor_local_jogo(jogo)
+        self.extractor_data_id(jogo)
+        self.extractor_mandante(jogo)
+        self.extractor_visitante(jogo)
+        self.extractor_placar(jogo)
+        self.extractor_detalhes(jogo)
 
     def organiza_jogos(self, lista):
         json = []
         jogos = lista.find_all('li')
         self.extractor_rodada(lista)
         for jogo in jogos:
-            self.extractor_local_jogo(jogo)
-            self.extractor_data_id(jogo)
-            self.extractor_mandante(jogo)
-            data = {'rodada': self.rodada, 'id': self.id, 'data_jogo': self.data, 'local_jogo': self.local, 'mandante' : self.mandante}
+            self.extractor_all(jogo)
+            data = {
+                'rodada': self.rodada, 'id': self.id, 'data_jogo': self.data,
+                'local_jogo': self.local, 'mandante': self.mandante, 'visitante': self.visitante,
+                'placar': self.placar, 'link_detalhes': self.link_detalhes
+            }
             json.append(data)
         return json
 
